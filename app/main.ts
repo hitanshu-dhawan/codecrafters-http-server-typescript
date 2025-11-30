6348649b4479576fa0a96388ccf7a14f97e27e8b
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2025-11-30 18:22:07 +0530

    Respond with 200

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,16 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
+
+    socket.on("data", () => {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.end();
+    });
+
     socket.on("close", () => {
         socket.end();
     });
+
 });
 
 server.listen(4221, "localhost");
